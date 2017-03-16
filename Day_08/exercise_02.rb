4638a52352c86aafcbc9d8da8c2e1cda68cb7bda
at-thapnguyen <thap.nguyen@asiantech.vn>
2017-03-16 22:54:35 +0700

    git create day_08

---
+++ Day_08/exercise_02.rb
@@ -0,0 +1,19 @@
+def exercise(num)
+sum = 0
+array = []
+if num >= 0
+    for i in 1..num
+      sum +=i
+      array.push(i)
+    end
+    print "0"
+    array.collect { |i|  print  " + " + i.to_s}
+    puts "=" + sum.to_s
+  else
+    puts "#{num} <0"
+  end
+end
+puts "Input number :"
+num = gets.chomp.to_i
+exercise num
+ 
---
+++ Day_08/exercise_03.rb
@@ -0,0 +1,7 @@
+def inputSquare(n)
+	array = []  
+	n.select{ |i| array.push(i) if i == Math.sqrt(i).to_i ** 2} 
+	  print array
+	end
+	  puts inputSquare(1..16)
+	
