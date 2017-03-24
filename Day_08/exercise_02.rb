743b680881a4eb010049c6ac5261d8644d10f050
at-thapnguyen <thap.nguyen@asiantech.vn>
2017-03-24 15:24:43 +0700

    update day_08

--- Day_08/exercise_02.rb
+++ Day_08/exercise_02.rb
@@ -1,13 +1,13 @@
 def exercise(num)
-sum = 0
-array = []
-if num >= 0
+  sum = 0
+  array = []
+  if num >= 0
     for i in 1..num
       sum +=i
       array.push(i)
     end
     print "0"
-    array.collect { |i|  print  " + " + i.to_s}
+    array.collect {|i| print " + " + i.to_s}
     puts "=" + sum.to_s
   else
     puts "#{num} <0"
