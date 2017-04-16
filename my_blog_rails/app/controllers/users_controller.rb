fafbb79dbd19ccd750fff27b8af0e7fb40b61abb
at-thapnguyen <thap.nguyen@asiantech.vn>
2017-04-16 20:54:14 +0700

    Exercise rails-CRUD

---
+++ my_blog_rails/app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+ def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save 
+        redirect_to users_path
+    else
+        render "new"
+    end
+  end
+  
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+	
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+  
+  
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(user_params)
+      # redirect_to  'show', :id => @user.id
+      redirect_to @user
+  	end
+	end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
+  end
+end
